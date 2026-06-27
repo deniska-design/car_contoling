31e395d069be2ece84376199133ea77a911be934
deniska-design <krutoidenis4321@gmail.com>
2026-06-27 14:30:04 +0200

    Update Car_motors.h

--- Car_motors.h
+++ Car_motors.h
@@ -10,61 +10,79 @@ typedef enum
   right   = 0,
 }direction;
 
+const int spining_speed = 240;     //degree per second
+const int driving_speed = 80;      //cm per second
+
+
 class car_motors 
 {
   int *motor_pins = new(int);
 public:
   car_motors(int pin1, int pin2, int pin3, int pin4);
+  int get_spining_speed();
+  int get_driving_speed();
   void move_directly();
   void stop();
   void move_rightly();
   void move_leftly();
-  void spin_drive(int degree, int distance, int spining_speed, int driving_speed, direction spining_direction);
+  void spin (int degree, direction spining_direction);
+  void drive(int distance);
 };
 
   car_motors::car_motors(int pin1, int pin2, int pin3, int pin4)
   {
-    motor_pins[1] = pin1;
-    motor_pins[2] = pin2;
-    motor_pins[3] = pin3;
-    motor_pins[4] = pin4;
+    motor_pins[0] = pin1;
+    motor_pins[1] = pin2;
+    motor_pins[2] = pin3;
+    motor_pins[3] = pin4;
   }
 
   void car_motors::move_directly()
   {
-    digitalWrite( motor_pins[1], LOW);
-    digitalWrite( motor_pins[2], HIGH);
-    digitalWrite( motor_pins[3], LOW);
-    digitalWrite( motor_pins[4], HIGH);
+    digitalWrite( motor_pins[0], LOW);
+    digitalWrite( motor_pins[1], HIGH);
+    digitalWrite( motor_pins[2], LOW);
+    digitalWrite( motor_pins[3], HIGH);
   }
 
   void car_motors::stop()
   {
+    digitalWrite( motor_pins[0],LOW );
     digitalWrite( motor_pins[1],LOW );
     digitalWrite( motor_pins[2],LOW );
     digitalWrite( motor_pins[3],LOW );
-    digitalWrite( motor_pins[4],LOW );
   }
 
   void car_motors::move_rightly()
   {
-    digitalWrite( motor_pins[1], LOW);        
+    digitalWrite( motor_pins[0], LOW);        
+    digitalWrite( motor_pins[1], HIGH);
     digitalWrite( motor_pins[2], HIGH);
-    digitalWrite( motor_pins[3], HIGH);
-    digitalWrite( motor_pins[4], LOW);
+    digitalWrite( motor_pins[3], LOW);
   }
 
   void car_motors::move_leftly()
-  {
-    digitalWrite( motor_pins[1], HIGH);     
+  {   
+    digitalWrite( motor_pins[0], HIGH);     
+    digitalWrite( motor_pins[1], LOW);
     digitalWrite( motor_pins[2], LOW);
-    digitalWrite( motor_pins[3], LOW);
-    digitalWrite( motor_pins[4], HIGH);
+    digitalWrite( motor_pins[3], HIGH);
   }
 
-  void car_motors::spin_drive(int degree, int distance, int spining_speed, int driving_speed, direction spining_direction)
+  int car_motors::get_spining_speed()
+  {
+    return spining_speed;
+  }
+
+  int car_motors::get_driving_speed()
+  {
+    return driving_speed;
+  }
+
+  void car_motors::spin (int degree, direction spining_direction)
   {
     int delay_time;
+    delay_time = (degree*1.0)/(spining_speed*1.0)*1000.0;
     if(spining_direction == right)
     {
       move_rightly();
@@ -72,12 +90,15 @@ public:
     {
       move_leftly();
     }
-    delay_time = (degree*1.0)/(spining_speed*1.0)*1000.0;
     delay(delay_time);
     stop();
-    delay(500);
-    move_directly();
+  }
+
+  void car_motors::drive(int distance)
+  {
+    int delay_time;
     delay_time = ((distance*1.0)/(driving_speed*1.0))*(1000*1.0);
+    move_directly();
     delay(delay_time);
     stop();
   }

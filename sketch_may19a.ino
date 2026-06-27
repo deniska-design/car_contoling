dbe1fc86bfc936158b5d2e2e6d52b63e122e5f7f
deniska-design <krutoidenis4321@gmail.com>
2026-06-27 14:29:21 +0200

    Refactor IR receiver and update signal handling

--- sketch_may19a.ino
+++ sketch_may19a.ino
@@ -1,59 +1,45 @@
-#include "IRremote.h"
-#include "Display.h"
+#include <IRremote.hpp>
 #include "Car_motors.h"
 
 #define SCL_Pin    A5  //Set clock pin to A5
 #define SDA_Pin    A4  //Set data pin to A4
-
-#define SIG_RIGHT   0xC23D  
-#define SIG_LEFT    0x22DD  
-#define SIG_DIRECT  0x629D  
-#define SIG_STOP    0xA857 
+#define SERVO_Pin  A3  
 
 enum
 {
-
-  degree_coefficient = 90,
-
-  spining_speed      = 240,     //degree per second
-  driving_speed      = 80,       //cm per second
-
-  HCtrig_pin = 12,
-  HCecho_pin = 13,
-  
   IRrecv_Pin = 3, 
-
 };
+
+const uint32_t SIG_RIGHT =  0x43;
+const uint32_t SIG_LEFT =   0x44;
+const uint32_t SIG_DIRECT = 0x46;
+const uint32_t SIG_STOP =   0x15;
 
 unsigned char bitmapArrowright[] = {  0x00,   0x00,   0x00,   0x00,   0x00, 0x00,   0x18,   0x24,   0x42,   0x81,   0x00,   0x00, 0x00,   0x00,   0x00,   0x00  };
 unsigned char bitmapArrowleft[] = {  0x00,   0x00,   0x00,   0x00,   0x00,   0x00,   0x81,   0x42,   0x24,   0x18,   0x00,   0x00,  0x00, 0x00,   0x00, 0x00  };
 unsigned char smile[] = { 0x00, 0x00, 0x1c, 0x02, 0x02, 0x02, 0x5c, 0x40, 0x40, 0x5c, 0x02, 0x02, 0x02, 0x1c, 0x00, 0x00 };
 
+uint32_t res;
+
 car_motors motors(5, 2, 4, 6); 
 
 Display display(8, 16, SCL_Pin, SDA_Pin);
 
-IRrecv irrecv (IRrecv_Pin);             // порт IR Sencor
-
 float way[3];
 
 void setup (){
-  servo.attach(SERVO_Pin);            // attaches the servo om pin 13 to the servo object
-
-  irrecv.enableIRIn();        //запускаем IR Sencor
-  
+  IrReceiver.begin(IRrecv_Pin, ENABLE_LED_FEEDBACK); // Start the receiver
   Serial.begin(9600);         //скорость передачи данных
 }
 
 void loop ()
 {  
-  
-  decode_results results;       //переменая куда помещается код с пульта 
-
-  if( irrecv.decode( &results ))        //если получили сигнал
+  if( IrReceiver.decode())        //если получили сигнал
   {         
-    int res = results.value;
-    Serial.println(res, HEX);          //в в терминал пишет какой код пришёл
+    IrReceiver.printIRResultShort(&Serial); // Print "old" raw data
+    res = IrReceiver.decodedIRData.command;
+    Serial.println(res, HEX); // Print "old" raw data
+    Serial.println(SIG_DIRECT, HEX); // Print "old" raw data
     if (res == SIG_LEFT){                //left
       motors.move_leftly();
       display.matrix_display(bitmapArrowleft);
@@ -70,6 +56,6 @@ void loop ()
       motors.stop();
       display.matrix_display(smile);
     }
-    irrecv.resume();                  //получаем опять сигнал
+    IrReceiver.resume();                  //получаем опять сигнал
   }
 } 
